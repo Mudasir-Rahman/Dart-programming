529e71d8a231e9169a96d2dfd1ce4491b413a594
Mudaser <mudaserrahman7@gmail.com>
2026-04-11 23:59:36 +0500

    Initial commit:late key

---
+++ let.dart
@@ -0,0 +1,12 @@
+late String name;
+
+void greating() {
+  name = 'Mudaser';
+}
+
+void main() {
+  greating(); // assigning value to the name variable
+  print(
+    "Hello $name",
+  ); // it will print Hello Mudaser because the name variable is initialized in the greating function
+}
