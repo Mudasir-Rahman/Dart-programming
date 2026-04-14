5115f1ca5deb46525805210b24d1737746ed4949
Mudaser <mudaserrahman7@gmail.com>
2026-04-15 00:10:22 +0500

    Initial commit:sum of a number using function perniterize

---
+++ perRe.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num1 = 5;
+  int num2 = 10;
+  int result = add(num1, num2);
+  print("The sum of $num1 and $num2 is: $result");
+}
+
+int add(int a, int b) {
+  int sum = a + b;
+  return sum;
+}
